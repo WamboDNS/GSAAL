56da387c41c961d5734892eb0113ca11353aeb35
Denis <deniswambold@gmail.com>
2024-01-23 19:35:22 +0100

    Add scripts to download all data required to perform our experiments.

---
+++ src/get_data.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+file_url="https://bwsyncandshare.kit.edu/s/KbPSN9g5KYoPWpw/download"
+destination_path="./experiments/datasets/"
+zip_file_name="elki.zip"
+extracted_folder_name="elki"
+
+mkdir -p "$destination_path"
+curl -o "${destination_path}${zip_file_name}" "$file_url"
+
+if [ $? -eq 0 ]; then
+    echo "Download completed successfully!"
+
+    # Extract the contents of the zip file
+    unzip -q "${destination_path}${zip_file_name}" -d "${destination_path}"
+
+    # Check if the extraction was successful
+    if [ $? -eq 0 ]; then
+        echo "Extraction completed successfully!"
+    else
+        echo "Error: Unable to extract the zip file."
+    fi
+
+    rm "${destination_path}${zip_file_name}"
+
+else
+    echo "Error: Unable to download the file."
+fi
+
+cd experiments && python3 Download_datasets.py
---
+++ src/get_synthetic_data.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+file_url="https://bwsyncandshare.kit.edu/s/k8PFJsXLwz8W8aR/download"
+destination_path="./"
+zip_file_name="synthetic_data.zip"
+extracted_folder_name="synthetic_data"
+
+mkdir -p "$destination_path"
+curl -o "${destination_path}${zip_file_name}" "$file_url"
+
+if [ $? -eq 0 ]; then
+    echo "Download completed successfully!"
+
+    # Extract the contents of the zip file
+    unzip -q "${destination_path}${zip_file_name}" -d "${destination_path}"
+
+    # Check if the extraction was successful
+    if [ $? -eq 0 ]; then
+        echo "Extraction completed successfully!"
+    else
+        echo "Error: Unable to extract the zip file."
+    fi
+
+    rm "${destination_path}${zip_file_name}"
+
+else
+    echo "Error: Unable to download the file."
+fi
